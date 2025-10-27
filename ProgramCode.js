3b05b42ad19aaf471c30cbd145abe9ac405aca8e
Costober <162455632+Costober@users.noreply.github.com>
2025-10-27 19:01:14 +0200

    Change double quotes to single quotes for PRODUCT_NAME

--- ProgramCode.js
+++ ProgramCode.js
@@ -3,7 +3,7 @@
  * Головний модуль для виведення назви програмного продукту.
  */
 
-const PRODUCT_NAME = "Integrated Stability Tracker";
+const PRODUCT_NAME = 'Integrated Stability Tracker';
 
 function displayProductName() {
   console.log(`Програмний продукт: ${PRODUCT_NAME}`);
