6d664c249cb270166444ab77e4e514e348431cac
Costober <162455632+Costober@users.noreply.github.com>
2025-10-27 18:24:35 +0200

    Create ProgramCode.js to display product name
    Added main module to display product name.

---
+++ ProgramCode.js
@@ -0,0 +1,12 @@
+/**
+ * ProgramCode.js
+ * Головний модуль для виведення назви програмного продукту.
+ */
+
+const PRODUCT_NAME = "Integrated Stability Tracker";
+
+function displayProductName() {
+  console.log(`Програмний продукт: ${PRODUCT_NAME}`);
+}
+
+displayProductName();
